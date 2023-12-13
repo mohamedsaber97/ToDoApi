813e2b8e00052d1f48bb594ae1d9031929f19fd1
Mohamed Saber <54990006+mohamedsaber97@users.noreply.github.com>
2023-12-13 14:37:05 +0200

    add function to get baseUrl and run environment in production or test

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -7,9 +7,26 @@ import static io.restassured.RestAssured.given;
 import io.restassured.http.ContentType;
 
 public class TestBase {
+
+    public static String getEnvironment() {
+        String env = System.getProperty("env", "production");
+        String baseUrl;
+        switch (env) {
+            case "production":
+                baseUrl = "https://qacart-todo.herokuapp.com/api/v1/";
+                break;
+            case "local":
+                baseUrl = "https://localhost:8888";
+                break;
+            default:
+                throw new RuntimeException("Environment is not supported");
+        }
+        return baseUrl;
+    }
+
     public static RequestSpecification getRequestSpec() {
         return given()
-                .baseUri("https://qacart-todo.herokuapp.com/api/v1/")
+                .baseUri(getEnvironment())
                 .contentType(ContentType.JSON)
                 .log().body();
     }
